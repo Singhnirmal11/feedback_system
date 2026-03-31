1123f9b6ffa8e3e7d3fafabd6a8cdba447d9b291
Singhnirmal11 <nirmal.nittinsaini@gmail.com>
2026-03-31 17:54:40 +0530

    Changes in facultyRoutes.js

--- server/routes/facultyRoutes.js
+++ server/routes/facultyRoutes.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
-
 const verifyToken = require("../middleware/verifyToken");
 const verifyAdmin = require("../middleware/verifyAdmin");
 
+router.get("/faculties", verifyToken, (req, res) => {
+  const query = "SELECT * FROM faculties";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error" });
+    }
+
+    res.json(results);
+  });
+});
 
 router.post("/faculties", verifyToken, verifyAdmin, (req, res) => {
   const { name, department } = req.body;
 
   if (!name || !department) {
-    return res.status(400).json({ message: "All fields are required" });
+    return res.status(400).json({ message: "Name and department are required" });
   }
 
-  const query = `
-    INSERT INTO faculties (name, department)
-    VALUES (?, ?)
-  `;
+  const query = "INSERT INTO faculties (name, department) VALUES (?, ?)";
 
   db.query(query, [name, department], (err, result) => {
     if (err) {
@@ -27,16 +34,39 @@ router.post("/faculties", verifyToken, verifyAdmin, (req, res) => {
   });
 });
 
+router.get("/faculties/:id/feedback", verifyToken, (req, res) => {
+  const faculty_id = req.params.id;
 
-router.get("/faculties", verifyToken, (req, res) => {
-  const query = "SELECT * FROM faculties";
+  const query = `
+    SELECT fb.id, f.name, f.department, fb.rating, fb.comment
+    FROM feedback fb
+    JOIN faculties f ON fb.faculty_id = f.id
+    WHERE f.id = ?
+  `;
 
-  db.query(query, (err, results) => {
+  db.query(query, [faculty_id], (err, results) => {
     if (err) {
+      console.log(err);
       return res.status(500).json({ message: "Database error" });
     }
 
-    res.status(200).json(results);
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Feedback not found" });
+    }
+
+    const totalRatings = results.length;
+    const averageRating =
+      results.reduce((sum, item) => sum + item.rating, 0) / totalRatings;
+
+    res.json({
+      faculty: {
+        name: results[0].name,
+        department: results[0].department,
+      },
+      totalFeedback: totalRatings,
+      averageFeedback: averageRating.toFixed(2),
+      feedback: results,
+    });
   });
 });
 
