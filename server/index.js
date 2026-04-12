8fe15578bfe5c96b22e1fc5ec80a58a4fa48f639
Keshavsaini22 <keshavsainikesu@gmail.com>
2026-04-12 11:21:33 +0530

    feat: initialize express server with authentication, authorization, and CRUD feedback routes

--- server/index.js
+++ server/index.js
@@ -21,13 +21,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin) || (origin && origin.endsWith(".vercel.app"))) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
